340ae041fc44716b2200cc2deb0684828c69013a
unknown <you@example.com>
2023-03-03 01:24:24 +1300

    first commit

---
+++ qrcode/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='qr-code-image' >
+       <img src='https://felixdiamond.github.io/qr-code-component-main/images/image-qr-code.png' alt='qrcode' />
+    </div>
+
+    <div className='contents'>
+      <h2>Improve your front-end skills by building projects</h2>
+      <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </div>
+    </div>
+  );
+}
+
+export default App;
